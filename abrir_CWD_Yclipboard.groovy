7ed0e629e724ad7adc1740428fabdff25b734799
Homar Orozco Apaza <homar.orozco@gmail.com>
2024-02-29 23:14:36 -0400

    Update abrir_CWD_Yclipboard.groovy
    current working directory can be opened in file explorer or website, also options to open fotos or video folders are included

--- abrir_CWD_Yclipboard.groovy
+++ abrir_CWD_Yclipboard.groovy
@@ -1,39 +1,54 @@
 //Abrir el mindmap presente ya sea con file Explorer o en el servidor Web
-import javax.swing.JOptionPane
+import javax.swing.*
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.awt.Desktop
 import java.net.URI
 
-String opcion = JOptionPane.showInputDialog(null, "(d)irectorio, (f)otos, (v)ideos, (w)eb")
+// Create the radio buttons
+def option1 = new JRadioButton("lcl/raiz")
+def option2 = new JRadioButton("lcl/fotos")
+def option3 = new JRadioButton("lcl/douga")
+def option4 = new JRadioButton("web/raiz")
+def option5 = new JRadioButton("web/fotos")
+def option6 = new JRadioButton("web/douga")
+// Group the radio buttons
+def group = new ButtonGroup()
+group.add(option1)
+group.add(option2)
+group.add(option3)
+group.add(option4)
+group.add(option5)
+group.add(option6)
+
+// Select the first option by default
+option1.setSelected(true)
+
+// Create a panel and add the radio buttons
+def panel = new JPanel()
+panel.add(option1)
+panel.add(option2)
+panel.add(option3)
+panel.add(option4)
+panel.add(option5)
+panel.add(option6)
+
+// Show the JOptionPane
+def opcion = JOptionPane.showConfirmDialog(null, panel, "Choose an option",
+        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE)
 
 Path mmpPathAbs = Paths.get(node.map.file.toString()).getParent();
-c.statusInfo = mmpPathAbs
-//textUtils.copyToClipboard(mmpPathAbs.toString())
 
 File f = new File(mmpPathAbs.toString())           // evitar cosas como ../..
 String linkPathCanonico = f.getCanonicalPath()      // D:\mmpsProg\Prog_Java\104_documentos\how_split_a_string.pdf
+String linkPathWeb = linkPathCanonico.replace("D:","http://localhost")
+linkPathWeb = linkPathWeb.replace("\\","/")        //http://192.168.1.12/miServer/how_split_a_string.pdf
+Desktop desktop = Desktop.getDesktop()    
 
-switch (opcion){
-    case 'd':
-        Desktop.getDesktop().open(new File(mmpPathAbs.toString()))
-        break
-    case 'f':
-        Desktop.getDesktop().open(new File(mmpPathAbs.toString() + "/0fotos"))
-        break
-    case 'v':
-        Desktop.getDesktop().open(new File(mmpPathAbs.toString() + "/0douga"))
-        break
-    case 'w':
-        String linkPathWeb = linkPathCanonico.replace("D:","http://localhost")
-        linkPathWeb = linkPathWeb.replace("\\","/")        //http://192.168.1.12/miServer/how_split_a_string.pdf
-        
-        if(Desktop.isDesktopSupported()){
-            Desktop desktop = Desktop.getDesktop()    
-            if(desktop.isSupported(Desktop.Action.BROWSE)){
-                desktop.browse(new URI(linkPathWeb))
-            }
-        }
-        break
-}
+if(option1.isSelected()){Desktop.getDesktop().open(new File(mmpPathAbs.toString()))}
+else if(option2.isSelected()){Desktop.getDesktop().open(new File(mmpPathAbs.toString() + "/0fotos"))}
+else if(option3.isSelected()){Desktop.getDesktop().open(new File(mmpPathAbs.toString() + "/0douga"))}
+else if(option4.isSelected()){desktop.browse(new URI(linkPathWeb))}
+else if(option5.isSelected()){desktop.browse(new URI("${linkPathWeb}/0fotos"))}
+else if(option6.isSelected()){desktop.browse(new URI("${linkPathWeb}/0douga"))}
 
