9b22692c89ced0710793aeb1eb2c96ccbb51737f
Homar Orozco Apaza <homar.orozco@gmail.com>
2024-02-29 21:38:40 -0400

    Update abrir_CWD_Yclipboard.groovy
    Now it's possible to open the folder in the fotos or video folders or open it in the web server

--- abrir_CWD_Yclipboard.groovy
+++ abrir_CWD_Yclipboard.groovy
@@ -1,11 +1,39 @@
+//Abrir el mindmap presente ya sea con file Explorer o en el servidor Web
+import javax.swing.JOptionPane
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.awt.Desktop
-import java.io.File;
+import java.net.URI
 
-//hallar el CWD de este mindmap
+String opcion = JOptionPane.showInputDialog(null, "(d)irectorio, (f)otos, (v)ideos, (w)eb")
+
 Path mmpPathAbs = Paths.get(node.map.file.toString()).getParent();
 c.statusInfo = mmpPathAbs
-textUtils.copyToClipboard(mmpPathAbs.toString())
+//textUtils.copyToClipboard(mmpPathAbs.toString())
 
-Desktop.getDesktop().open(new File(mmpPathAbs.toString()))
+File f = new File(mmpPathAbs.toString())           // evitar cosas como ../..
+String linkPathCanonico = f.getCanonicalPath()      // D:\mmpsProg\Prog_Java\104_documentos\how_split_a_string.pdf
+
+switch (opcion){
+    case 'd':
+        Desktop.getDesktop().open(new File(mmpPathAbs.toString()))
+        break
+    case 'f':
+        Desktop.getDesktop().open(new File(mmpPathAbs.toString() + "/0fotos"))
+        break
+    case 'v':
+        Desktop.getDesktop().open(new File(mmpPathAbs.toString() + "/0douga"))
+        break
+    case 'w':
+        String linkPathWeb = linkPathCanonico.replace("D:","http://localhost")
+        linkPathWeb = linkPathWeb.replace("\\","/")        //http://192.168.1.12/miServer/how_split_a_string.pdf
+        
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop()    
+            if(desktop.isSupported(Desktop.Action.BROWSE)){
+                desktop.browse(new URI(linkPathWeb))
+            }
+        }
+        break
+}
+
