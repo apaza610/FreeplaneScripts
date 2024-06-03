f0f596cd6e379a821842c91173f8e7829567f00d
Homar Orozco Apaza <homar.orozco@gmail.com>
2024-06-02 23:36:30 -0400

    Update abrir_LINK_ComoLocal.groovy

--- abrir_LINK_ComoLocal.groovy
+++ abrir_LINK_ComoLocal.groovy
@@ -2,6 +2,8 @@ import java.awt.Desktop;
 import java.io.File;
 
 print("\n-------------------------------------\n")
+print("\n-------------------------------------\n")
+print("\n-------------------------------------\n")
 String linkPathWeb = node.link.text.toString()  // http://192.168.1.12/miServer/mmpsProg/Prog_Java/104_documentos/how_split_a_string.pdf
 print(linkPathWeb)
 print("\n-------------------------------------\n")
